daa42b9970394cb7a50e3ea428d6af82e21b0de5
Max <shadow.iqka@qq.com>
2023-04-13 17:19:59 +0800

    [add] stream api

--- scripts/qna.js
+++ scripts/qna.js
@@ -102,6 +102,29 @@ function Answer(question, user, sid) {
   return { sid: sid, content: message.content };
 }
 
+/**
+ * yao run scripts.qna.Stream "帮我写一个关于心血管健康的论文" 张三 371182ee-d76e-4680-a2b7-469cd1020041
+ *
+ * @param {*} messages
+ * @param {*} ctx
+ * @returns
+ */
+function Stream(question, user, sid) {
+  let messages = [{ role: "user", content: question }];
+  let cfg = setting();
+  let url = `${cfg.host}/v1/chat/completions`;
+  messages = messages || [];
+  let paylad = { model: cfg.model, messages: messages, user: sid };
+  return stream(url, paylad, cfg.key, (data) => {
+    ssEvent("message", data.slice(5));
+    if (data == "data: [DONE]") {
+      cancel();
+      return 0;
+    }
+    return 1;
+  });
+}
+
 // === utils =================================
 
 /**
@@ -164,4 +187,52 @@ function getSummaries(docs) {
   });
 
   return summaries;
+}
+
+/**
+ * Post to openai data stream
+ * @param {*} url
+ * @param {*} payload
+ * @param {*} key
+ * @returns
+ */
+function stream(url, payload, key, cb) {
+  payload["stream"] = true;
+  let response = http.Stream("POST", url, cb, payload, null, {
+    Authorization: `Bearer ${key}`,
+    "Content-Type": "application/json; charset=utf-8",
+  });
+
+  if (response.code != 200) {
+    let data = response.data || {};
+    let err = data.error || {};
+    let message = err.message || "unknown error";
+    log.Error(`OpenAI API error ${message}`);
+    throw new Exception(`OpenAI API error ${message}`, response.code || 500);
+  }
+
+  return response.data;
+}
+
+/**
+ * Get openai setting
+ * @returns {Map}
+ */
+function setting() {
+  let vars = Process(
+    "utils.env.GetMany",
+    "OPENAI_MODEL",
+    "OPENAI_KEY",
+    "OPENAI_PROXY",
+    "OPENAI_MODEL_EMBEDDING"
+  );
+
+  return {
+    model: vars["OPENAI_MODEL"] || "gpt-3.5-turbo",
+    model_embedding: vars["OPENAI_MODEL_EMBEDDING"] || "text-embedding-ada-002",
+    key: vars["OPENAI_KEY"],
+    host: vars["OPENAI_PROXY"]
+      ? vars["OPENAI_PROXY"]
+      : "https://api.openai.com",
+  };
 }
